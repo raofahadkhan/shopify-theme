ac1dac6ffd47ab5fe4522786ff13cbf9a646c1b7
RAO FAHAD <raofahad046@gmail.com>
2023-04-05 00:58:59 +0500

    updated context to use cart section

--- src/components/shared/CartContext.tsx
+++ src/components/shared/CartContext.tsx
@@ -1,20 +1,79 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { ProductDataType } from "../typesandArrays/AllMensData";
 import { contextType } from "../typesandArrays/creatContextType";
 import { ReactNode } from "react";
 
 export const CartContext = createContext<contextType | null>(null);
 
+const queryForGettingAllProducts = `{
+  products(first:25) {
+    edges {
+      node {
+          variants(first:250){
+              edges{
+                  node{
+                      id,
+                      title,
+                      price{
+                          amount,
+                          currencyCode
+                      },
+                      sku
+
+
+                  }
+              }
+          }
+          handle
+          images(first:3){
+              edges{
+                  node{
+                      url
+                  }
+              }
+          },
+        id,
+        title,
+        description
+      }
+    }
+  }
+}
+`;
+
+const getAllProductsDataFromAPI = async () => {
+  const url = "https://ecomshoptheme.myshopify.com/api/2023-01/graphql.json";
+  let response = await fetch(url, {
+    method: "Post",
+    headers: {
+      "Content-type": "application/json",
+      "X-Shopify-Storefront-Access-Token": `${process.env.API_KEY}`,
+    },
+    body: JSON.stringify({ query: queryForGettingAllProducts }),
+  });
+  return response.json();
+};
+
 export const CartProvider = ({ children }: { children: ReactNode }) => {
   const [cart, setCart]: any = useState([]);
   const [navbarcolor, setNavbarcolors] = useState(false);
   const [price, setPrice] = useState(0);
+  const [allProductData, setAllProductData] = useState();
+  const data = async () => {
+    const dataa = await getAllProductsDataFromAPI();
+    console.log(dataa);
+    setAllProductData(dataa);
+  };
+  useEffect(() => {
+    data();
+  }, []);
+
   const addToCart = (item: ProductDataType) => {
     setCart([...cart, item]);
     setPrice(Number(item.price));
   };
   function setNavbarcolor(item: boolean) {
-    setNavbarcolors(item)
+    setNavbarcolors(item);
   }
   function updatePrice(action: string, updatedPrice: string) {
     if (action === "addition") {
@@ -32,7 +91,16 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
   };
   return (
     <CartContext.Provider
-      value={{ cart, addToCart, removeFromCart, price, updatePrice, navbarcolor, setNavbarcolor }}
+      value={{
+        cart,
+        addToCart,
+        removeFromCart,
+        price,
+        updatePrice,
+        navbarcolor,
+        setNavbarcolor,
+        allProductData,
+      }}
     >
       {children}
     </CartContext.Provider>
